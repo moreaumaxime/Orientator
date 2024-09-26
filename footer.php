ccb987a2cb0146d17870ba3a480f45030a44607d
moreaumaxime <moreaum@3il.fr>
2024-09-26 11:02:37 +0200

    Create footer.php
    a inclure en bas d'un layout

---
+++ footer.php
@@ -0,0 +1,14 @@
+
+<div class="mentions">
+    <h2>Nous contacter </h2>
+    <ul>
+        <li>addresse : 1 rue de la perdrix, Limoges</li>
+        <li>telephone: 01 01 01 01 01</li>
+        <li>propriétaire: John Doe</li>
+    </ul>
+</div>
+
+
+
+</body>
+</html>
