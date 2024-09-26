43838d8ff57d5e855ba3849b1c5886cd499727de
ronanboot <158291137+ronanboot@users.noreply.github.com>
2024-09-26 11:21:45 +0200

    Create barre.php
    Barre de navigation

---
+++ barre.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<nav>
+    <ul>
+        <li><a href="#">Accueil</a></li>
+        <li><a href="#">EX</a></li>
+        <li><a href="#">EX</a></li>
+        <li><a href="#">Questionnaire</a></li>
+    </ul>
+</nav>
