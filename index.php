8d531063da28a2a4e3bfba2064febe717c66dc6a
ronanboot <158291137+ronanboot@users.noreply.github.com>
2024-09-26 13:58:56 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,47 @@
 <!DOCTYPE html>
-<html>
+<html lang="fr">
     <head>
-        <title>My Website</title>
+        <title>Orientator</title>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+       
+        <!-- Fichiers CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+        <link href='http://fonts.googleapis.com/css?family=Holtwood+One+SC' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="css/styles.css">
+
+        <!-- Fichiers javscript -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     </head>
-    <body>
+
+    <body> 
         <?php include 'navbar.php'; ?>
-        <h1>Welcome to My Website</h1>
-        <p>This is the homepage content.</p>
+
+        <div class="container site">
+            <h2 class="text-title2">Description</h2>
+
+
+        </div>
+        <div class="container site">
+        <h2 class="text-title2">Formations</h2>
+
+
+        </div>
+        <div class="container site">
+        <h2 class="text-title2">Entreprises</h2>
+
+
+        </div>
+        <div class="container site">
+        <h2 class="text-title2">Métiers</h2>
+
+
+        </div>
+        <div class="container site">
+        <h2 class="text-title2">Autres ressources</h2>
+
+
+        </div>
     </body>
 </html>
