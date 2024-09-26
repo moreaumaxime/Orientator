db9b6406aca8528d437354aaa28d18fa745ec608
ronanboot <158291137+ronanboot@users.noreply.github.com>
2024-09-26 11:24:06 +0200

    Delete barre.php

--- barre.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<nav>
-    <ul>
-        <li><a href="#">Accueil</a></li>
-        <li><a href="#">EX</a></li>
-        <li><a href="#">EX</a></li>
-        <li><a href="#">Questionnaire</a></li>
-    </ul>
-</nav>
