e0c328f3a6bc420fa7194884a66a66ff7b0c7a3a
ronanboot <158291137+ronanboot@users.noreply.github.com>
2024-09-26 13:52:40 +0200

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -1,9 +1,38 @@
 <!DOCTYPE html>
-<nav>
-    <ul>
-        <li><a href="#">Accueil</a></li>
-        <li><a href="#">EX</a></li>
-        <li><a href="#">EX</a></li>
-        <li><a href="#">Questionnaire</a></li>
-    </ul>
-</nav>
+<meta charset="utf-8">
+        <link rel="stylesheet" href="style.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto">
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+<div align="center">
+    <h1 class="text-title">Orientator</h1>
+    <nav>
+        <ul>
+            <li>
+                <i class="fa fa-home" aria-hidden="true"></i> 
+                Accueil
+                <div class="underline"></div>
+            </li>
+            <li>
+                <i class="fa fa-file-text-o" aria-hidden="true"></i> 
+                Branches 1
+                <div class="underline"></div>
+            </li>
+            <li>
+                <i class="fa fa-file-text-o" aria-hidden="true"></i> 
+                Branches 2
+                <div class="underline"></div>
+            </li>
+            <li>
+                <i class="fa fa-file-text-o" aria-hidden="true"></i> 
+                Branches 3
+                <div class="underline"></div>
+            </li>
+            <li>
+                <i class="fa fa-info-circle" aria-hidden="true"></i> 
+                Questionnaire
+                <div class="underline"></div>
+            </li>
+        </ul>
+    </nav>
+</div>
