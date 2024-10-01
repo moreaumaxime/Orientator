e015f9fd54d43eac339258de6bc07c3cc329ff9f
ronanboot <158291137+ronanboot@users.noreply.github.com>
2024-10-01 13:44:48 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,13 @@
-
+<!DOCTYPE html>
+<meta charset="utf-8">
+        <link rel="stylesheet" href="styles.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto">
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+<div align="center">
+    
 <div class="mentions">
-    <h2>Nous contacter </h2>
+    <h2>Nous contacter</h2>
     <ul>
         <li>addresse : 1 rue de la perdrix, Limoges</li>
         <li>telephone: 01 01 01 01 01</li>
