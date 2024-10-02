c86dc70022e8fb9b2f8d52ab4507999a098af4f8
ronanboot <158291137+ronanboot@users.noreply.github.com>
2024-10-02 15:08:40 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -5,12 +5,14 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto">
         <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 <div align="center">
-    
-<div class="mentions">
-    <h2>Nous contacter</h2>
-    <ul>
-        <li>addresse : 1 rue de la perdrix, Limoges</li>
-        <li>telephone: 01 01 01 01 01</li>
-        <li>propriétaire: John Doe</li>
-    </ul>
-</div>
+<footer>
+    <div class="mentions">
+        <h2>Nous contacter</h2>
+        <ul class="ul-foot">
+            <li class="li-foot">addresse : 1 rue de la perdrix, Limoges</li>
+            <li class="li-foot">telephone: 01 01 01 01 01</li>
+            <li class="li-foot">propriétaire: John Doe</li>
+        </ul>
+    </div>
+</footer>
+
