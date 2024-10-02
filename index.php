a53422552900cbf3d2021ab4a20cacf020d8171a
ronanboot <158291137+ronanboot@users.noreply.github.com>
2024-10-02 16:10:29 +0200

    Update index.php

--- index.php
+++ index.php
@@ -25,8 +25,4 @@ if (isset($_GET['page']) && $_GET['page'] !== '') {
 }
 
 
-<<<<<<< HEAD
 ?>
-=======
-?>
->>>>>>> parent of fa1f6e3 (Merge branch 'main' of https://github.com/moreaumaxime/Orientator)
