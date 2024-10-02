a8c2a6c8132e1eb027b9fddfaf47da1c1551558a
ronanboot <158291137+ronanboot@users.noreply.github.com>
2024-10-02 15:18:29 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,39 +1,28 @@
-<!DOCTYPE html>
-<html lang="fr">
-    <head>
-        <title>Orientator</title>
-        <meta charset="utf-8"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-       
-        <!-- Fichiers CSS -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-        <link href='http://fonts.googleapis.com/css?family=Holtwood+One+SC' rel='stylesheet' type='text/css'>
-        <link rel="stylesheet" href="css/styles.css">
+<?php
 
-        <!-- Fichiers javscript -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    </head>
 
-    <body> 
-        <?php include 'navbar.php'; ?>
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-6" back>
-                <h2 class="text-title2">Branche</h2>
-                <u class="text">Et est admodum mirum videre plebem innumeram mentibus ardore quodam infuso cum dimicationum curulium eventu pendentem. haec similiaque memorabile nihil vel serium agi Romae permittunt. ergo redeundum ad textum.Quare hoc quidem praeceptum, cuiuscumque est, ad tollendam amicitiam valet; illud potius praecipiendum fuit, ut eam diligentiam adhiberemus in amicitiis comparandis, ut ne quando amare inciperemus eum, quem aliquando odisse possemus. Quin etiam si minus felices in diligendo fuissemus, ferendum id Scipio potius quam inimicitiarum tempus cogitandum putabat.</u>
-            </div>
-            <div class="col-sm-6">
-                <h2 class="text-slogan">Slogan</h2>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-sm">col-sm</div>
-            <div class="col-sm">col-sm</div>
-            <div class="col-sm">col-sm</div>
-        </div>
-    </div>
-</div>
-  
-    </body>
-</html>
+require_once('controllers/controllerAccueil.php');
+require_once('controllers/controllerBranches.php');
+
+
+
+if (isset($_GET['page']) && $_GET['page'] !== '') {
+	if ($_GET['page'] === 'accueil') {
+    	if (isset($_GET['id']) && $_GET['id'] > 0) {
+        	$identifier = $_GET['id'];
+
+        	post($identifier);
+    	} else {
+        	echo 'Erreur : aucun identifiant de billet envoyé';
+
+        	die;
+    	}
+	} else {
+    	echo "Erreur 404 : la page que vous recherchez n'existe pas.";
+	}
+} else {
+	Accueil();
+}
+
+
+?>
