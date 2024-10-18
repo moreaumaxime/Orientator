b759753aabe9b9d45b1451a10ca556953c1e5b1c
ronanboot <158291137+ronanboot@users.noreply.github.com>
2024-10-18 16:14:05 +0200

    Update branches.php

--- layouts/branches.php
+++ layouts/branches.php
@@ -1,76 +1,69 @@
-<?php require("header.php") ?>
-
+<?php require("header.php"); ?>
 <html>
-    <head>
-        <meta charset="utf-8" />
-        <title>Orientateur</title>
-        <link rel="icon" type="image/png" href="/images/OrientatorLogo.png">
-        <link href="css/styles.css" rel="stylesheet" />
-    </head>
+<head>
+    <meta charset="utf-8" />
+    <title>Détails de la Branche</title>
+    <link rel="icon" type="image/png" href="/images/OrientatorLogo.png">
+    <link href="css/styles.css" rel="stylesheet" />
+</head>
 
 <body>
-    <?php 
-    require_once 'controllers\controllerBranches.php';
-    $statement = $db->query('
-        SELECT filiere_nom, filière.Slogan, filiere_description, entreprise_nom, entreprise_description, formation_nom, formation_description, emploi_descriptionFROM filière
-        LEFT JOIN Asso3 ON filière.ID = Asso3.filiere_id
-        LEFT JOIN entreprises ON Asso3.entreprise_id = entreprises.ID
-        LEFT JOIN Asso1 ON filière.ID = Asso1.filiere_id
-        LEFT JOIN formations ON Asso1.formation_id = formations.ID
-        LEFT JOIN Asso2 ON filière.ID = Asso2.filiere_id
-        LEFT JOIN emploi ON Asso2.emploi_id = emploi.ID
-    ');
-
-    while($filiere = $statement->fetch()) 
-    {
-        echo '
     <div class="container">    
-        <div class="row">
-            <div class="col-sm-6" back>
-                <h2 class="text-title2">' . htmlspecialchars($filiere['filiere_nom']) . '</h2>
+        <?php 
+        if ($filiere) {
+            echo '
+            <div class="row">
+                <div class="col-sm-6">
+                    <h2 class="text-title2">' . htmlspecialchars($filiere['FiliereNom']) . '</h2>
+                </div>
+                <div class="col-sm-6">
+                    <h2 class="text-slogan">' . htmlspecialchars($filiere['FiliereSlogan']) . '</h2>
+                </div> 
             </div>
-            <div class="col-sm-6">
-                <h2 class="text-slogan">'. htmlspecialchars($filiere['Slogan']) . '</h2>
-            </div> 
-        </div>
-        <div class="row">
-            <div class="col-sm-6" back>  
-                <h2 class="text-title2">Description :</h2>
-                <u class="text">' . htmlspecialchars($filiere['filiere_description']) . '</u>
-            </div>
-            <div class="col-sm-6">
+            <div class="row">
+                <div class="col-sm-6">  
+                    <h2 class="text-title2">Description :</h2>
+                    <u class="text">' . htmlspecialchars($filiere['FiliereDescription']) . '</u>
+                </div>
+                <div class="col-sm-6">
                     <h2 class="text-title2">Entreprises :</h2>
                     <ul>';
-                    foreach ($filiere['entreprises'] as $entreprise) {
-                        echo '<li><strong>' . htmlspecialchars($entreprise['nom']) . ':</strong> ' . htmlspecialchars($entreprise['description']) . '</li>';
+                    if ($filiere['EntrepriseNom']) {
+                        echo '<li><strong>' . htmlspecialchars($filiere['EntrepriseNom']) . ':</strong> ' . htmlspecialchars($filiere['EntrepriseDescription']) . '</li>';
+                    } else {
+                        echo '<li>Aucune entreprise disponible.</li>';
                     }
                     echo '</ul>
+                </div>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-sm-6">
-                <h2 class="text-title2">Formations :</h2>
-                <ul>';
-                foreach ($filiere['formations'] as $formation) {
-                    echo '<li><strong>' . htmlspecialchars($formation['nom']) . ':</strong> ' . htmlspecialchars($formation['description']) . '</li>';
-                }
-                echo '</ul>
-            </div>
-            <div class="col-sm-6">
-                <h2 class="text-title2">Emplois :</h2>
-                <ul>';
-                foreach ($filiere['emplois'] as $emploi) {
-                    echo '<li>' . htmlspecialchars($emploi) . '</li>';
-                }
-                echo '</ul>
-            </div>
-        </div>
-    </div>';
-    }
-    ?>
-        
+            <div class="row">
+                <div class="col-sm-6">
+                    <h2 class="text-title2">Formations :</h2>
+                    <ul>';
+                    if ($filiere['FormationNom']) {
+                        echo '<li><strong>' . htmlspecialchars($filiere['FormationNom']) . ':</strong> ' . htmlspecialchars($filiere['FormationDescription']) . '</li>';
+                    } else {
+                        echo '<li>Aucune formation disponible.</li>';
+                    }
+                    echo '</ul>
+                </div>
+                <div class="col-sm-6">
+                    <h2 class="text-title2">Emplois :</h2>
+                    <ul>';
+                    if ($filiere['EmploiNom']) {
+                        echo '<li><strong>' . htmlspecialchars($filiere['EmploiNom']) . ':</strong> ' . htmlspecialchars($filiere['EmploiDescription']) . '</li>';
+                    } else {
+                        echo '<li>Aucun emploi disponible.</li>';
+                    }
+                    echo '</ul>
+                </div>
+            </div>';
+        } else {
+            echo "<p>Aucune filière trouvée pour l'ID spécifié.</p>";
+        }
+        ?>
+    </div>
 </body>
 
-<?php require("footer.php") ?>
-
+<?php require("footer.php"); ?>
 </html>
